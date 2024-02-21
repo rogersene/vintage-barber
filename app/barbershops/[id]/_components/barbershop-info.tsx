1c4de6586641525c2037b9613ad7b845a5717acf
Roger Crescencio <rogercrescencio@MacBook-Air-de-Roger.local>
2024-02-21 06:01:03 -0300

    feat: add side menu component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,7 +1,9 @@
 "use client"
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet,SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
-import { ChevronLeftIcon, MapPin, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -23,9 +25,19 @@ const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
                 <ChevronLeftIcon />
             </Button>
 
-            <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-                <MenuIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+                
+                <SheetContent className="p-0">
+                    <SideMenu />
+                </SheetContent>
+            </Sheet>
+
+
             <Image 
                 alt={barbershop.name} 
                 fill 
